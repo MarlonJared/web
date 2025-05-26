29805dad66d4b6f86feae8e04b428af374bb434b
MarlonJared <jaredospina2013@gmail.com>
2025-05-26 20:25:47 +0200

    actualizacion de contacto

---
+++ script_redes.js
@@ -0,0 +1,8 @@
+
+  // Cargar script oficial de Instagram para el embed
+(function () {
+  const script = document.createElement('script');
+  script.async = true;
+  script.src = 'https://www.instagram.com/embed.js';
+  document.body.appendChild(script);
+})();
